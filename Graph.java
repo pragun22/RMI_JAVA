fccdff34b2dc8eadf3631e6fc5d724640ccae59d
pragun22 <saxenapragun@gmail.com>
2020-02-14 05:31:04 -0800

    working on logic

--- Graph.java
+++ Graph.java
@@ -7,19 +7,36 @@ import java.rmi.*;
 import java.rmi.server.*;
 import java.rmi.registry.*; 
 
-public class Graph extends UnicastRemoteObject{
+public class Graph extends UnicastRemoteObject {
+    Graph() throws RemoteException { 
+        super(); 
+    } 
     Map<String,Integer> Graphs = new HashMap<String, Integer>(); 
+    Map<String, HashMap<Integer, ArrayList<Integer[]> > > adj = new HashMap<String, HashMap<Integer, ArrayList<Integer[]> > >(); 
+    // Map<String, Map<Integer, ArrayList<Integer> > > adj = new HashMap<String, Map<Integer, ArrayList<Integer> > >(); 
     public String add_graph(String graphName, Integer n){
-        if(Graph.containsKey(graphName)){
+        if(Graphs.containsKey(graphName)){
             return "A graph already with this name exist";
         }
-        Graph.put(graphName, n);
+        Graphs.put(graphName, n);
+        adj.put(graphName, new HashMap<Integer, ArrayList<Integer[]> >());
+        // adj.put(graphName, new HashMap<Integer, ArrayList<Integer> >());
         return "Graph Added Succesfully\n";
     }
     public String add_edge(String graphName, Integer u, Integer v, Integer w){
-        if(!Graph.containsKey(graphName)){
+        if(!Graphs.containsKey(graphName)){
             return "Sorry no such graph exist";
         }
-        
+        ArrayList<Integer[]> list1 = adj.get(graphName).get(u);
+        ArrayList<Integer[]> list2 = adj.get(graphName).get(v);
+        // HashMap<Integer, ArrayList<Integer> > adj = adj.get(graphName);
+        list1.add(new Integer[]{v ,w}); 
+        list2.add(new Integer[]{u ,w}); 
+        return "";
     }
+
 } 
+class pair{
+    public Integer first;
+    public Integer second;
+}
